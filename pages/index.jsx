9c4ef17cadc9f299610c52b0c31c907079fdcd97
Vox Vista <voxvista@voxvista.com>
2020-06-04 20:04:38 +0530

    Initial Commit

---
+++ pages/index.jsx
@@ -0,0 +1,53 @@
+import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I'm a Web Developer/ Designer with over 8 years of expertise in with
+          success developing web applications.
+        </p>
+        <p>
+          My expertise is in React JS, Vue, Laravel, Codeignitor, Magento,
+          Wordpress. I have designed and developed everything from simple web
+          pages to intricate e-commerce websites.{" "}
+        </p>
+        <p>I have excellent communication skills.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
